8dbe319cb127c9b3973bc3918ff2a8d1123154cc
Qubert <upafourth@gmail.com>
2017-12-19 10:50:39 +0100

    Add project files.

---
+++ CTCI 3.6 Sorted Stack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_3._6_Sorted_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
